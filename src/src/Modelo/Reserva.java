e5108a48dbc4bf5930e4d7945464dee976bb7799
Matias Antonelli <matiasantonelli6874@gmail.com>
2024-05-23 22:22:31 -0300

    atributos clase Reserva

--- src/src/Modelo/Reserva.java
+++ src/src/Modelo/Reserva.java
@@ -1,7 +1,11 @@
 package Modelo;
 
 public class Reserva{
-
+    //atributos
+    private Alojamiento alojamiento;
+    private Cliente cliente;
+    private double precioTotal;
+    private int pin; //id
 
 
     //hacer funcion que copie las fechas que tiene guardado cliente en alojamiento
