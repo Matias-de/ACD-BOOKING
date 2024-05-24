f2b1a1fd4044b6b417e03576b580b03c55fe01c2
Andres Canevello <canevelo0o@gmail.com>
2024-05-24 16:31:11 -0300

    clase booking acd
    se agregaron atributos, constructores y metodos a la clase booking acd

--- src/src/Modelo/BookingACD.java
+++ src/src/Modelo/BookingACD.java
@@ -1,10 +1,39 @@
 package Modelo;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class BookingACD{
+    //atributos
+    HashMap <Cliente, HashSet<Reserva>> hashMapCliente;
+    HashMap <Alojamiento, HashSet<Reserva>> hashMapAlojamiento;
 
+    HashSet <Cliente> clienteHashSet; //Guardariamos un JSON
+    HashSet <Alojamiento> alojamientoHashSet; //guardariamos un JSON
+    HashSet <Reserva> reservaHashSet; //guardariamos en Archivo
 
+    //constructor
+    public BookingACD()
+    {
+        hashMapCliente = new HashMap<Cliente,HashSet<Reserva>>();
+        hashMapAlojamiento = new HashMap<Alojamiento, HashSet<Reserva>>();
+        clienteHashSet = new HashSet<Cliente>();
+        alojamientoHashSet = new HashSet<Alojamiento>();
+        reservaHashSet = new HashSet<Reserva>();
+    }
 
-
+    public void agregarCliente(Cliente nuevoCliente)
+    {
+        clienteHashSet.add(nuevoCliente);
+    }
+    public void agregarAlojamiento(Alojamiento nuevoAlojamiento)
+    {
+        alojamientoHashSet.add(nuevoAlojamiento);
+    }
+    public void cargarReserva(Reserva nuevaReserva)
+    {
+        reservaHashSet.add(nuevaReserva);
+    }
 
 
 }
