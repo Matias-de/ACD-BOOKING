a8f0fd02c8e5f13ca775563d5ee2d0a4a187c5b3
Matias Antonelli <matiasantonelli6874@gmail.com>
2024-05-23 22:48:35 -0300

    Constructores agregados a clase Reserva

--- src/src/Modelo/Reserva.java
+++ src/src/Modelo/Reserva.java
@@ -1,11 +1,30 @@
 package Modelo;
 
+import java.util.UUID;
+
 public class Reserva{
     //atributos
     private Alojamiento alojamiento;
     private Cliente cliente;
     private double precioTotal;
-    private int pin; //id
+    private int pin; //buscar como aumentarse y no repetirse (archivos quizas?)
+
+    //constructores
+
+    public Reserva() {
+        alojamiento= null;
+        cliente= new Cliente();
+        precioTotal=0;
+        pin=0;
+    }
+
+    public Reserva(Alojamiento alojamiento, Cliente cliente, double precioTotal) {
+        this.alojamiento = alojamiento;
+        this.cliente = cliente;
+        this.precioTotal = precioTotal;
+        pin=0;
+    }
+
 
 
     //hacer funcion que copie las fechas que tiene guardado cliente en alojamiento
