ce37d060b98e9dc3ee042a4a424af96be812395c
Matias Antonelli <matiasantonelli6874@gmail.com>
2024-05-24 22:18:52 -0300

    Update IOperaciones.java
    se agregaron carga y listado genericos a la Interfaz

--- src/src/Interfaces/IOperaciones.java
+++ src/src/Interfaces/IOperaciones.java
@@ -1,7 +1,16 @@
 package Interfaces;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public interface IOperaciones<E>{
 
+    void cargarHashMap(HashMap<E,E> hashMap);//carga un hashmap
+    void listarHashMap(HashMap<E,E> hashMap);//muestra un hashmap
+
+
+    void cargarHashSet(HashSet<E> hashSet);//carga un hashset
+    void listarHashSet(HashSet<E> hashSet);//muestra un hashset
 
 
 }
