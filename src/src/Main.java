29bf08e1aee24f10cac81b8901359a46c89e1f6a
Andres Canevello <canevelo0o@gmail.com>
2024-05-28 18:32:40 -0300

    agregamos menu
    realizamos el diagrame del menu para i probando cosas

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,8 +1,10 @@
+import Modelo.Alojamiento;
 import Modelo.BookingACD;
 import Modelo.Cliente;
 import Modelo.Reserva;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -19,14 +21,49 @@ public class Main {
         opc = scan.nextInt(); // cargamos la opcion elegida por el administrador
         switch (opc)
         {
-            case 1:
-                nuevoBooking.cargarHashMap();
+            case 1: // me gustaria que la opcion 1 te deje cargar clientes, la 2 alojamientos y asi...
+
+                Cliente nuevoCliente = cargaCliente();
+                Alojamiento nuevoAlojamiento = cargarAlojamiento();
+                Reserva nuevaReserva = nuevoBooking.reservar(nuevoCliente,nuevoAlojamiento); //necesito q esta funcion retorne null o la reserva si se pudo realizar
+
+                nuevoBooking.agregarCliente(nuevoCliente,nuevaReserva); //necesito la reserva para mandarla a esta funcion
+                nuevoBooking.agregarAlojamiento(nuevoAlojamiento,nuevaReserva);// necesito la reserva para mandarla a esta funcion
 
 
         }
     }
-    public static void menu(int opc)
+    public static Alojamiento cargarAlojamiento() //soy andru me falta terminar a la noche lo termino
     {
+        System.out.println("Ingrese nombre");
+    }
+    public static Cliente cargaCliente()
+    {
+        String nombre,apellido,email,medioDePago;
+        int cantDePersonas=0,diaInicio=0,diaFin=0,mesInicio=0,mesFin=0;
+        boolean añoInicio, añoFin;
 
+        System.out.println("Ingrese nombre: ");
+        nombre = scan.nextLine();
+        System.out.println("Ingrese apellido:");
+        apellido = scan.nextLine();
+        System.out.println("Ingrese mail:");
+        email = scan.nextLine();
+        System.out.println("Ingrese medio de pago (Tarjeta o efectivo):");
+        medioDePago = scan.nextLine();
+        System.out.println("Ingrese la cantidad de personas:");//deberiamos de saber si hay niños
+       cantDePersonas = scan.nextInt();
+        System.out.println("Ingrese cuando va a ingresar a la propiedad en orden de DIA, MES, Y AÑO(maximo 1 año):"); //como nosotros vamos a ser los que cargan clientes y demas vamos a asignar
+        diaInicio = scan.nextInt();                                                 //el valor del booleano que nos dice si el año es el actual o el siguiente
+        mesInicio = scan.nextInt();
+        añoInicio = scan.nextBoolean();
+
+        System.out.println("Ingrese cuando se va a retirar de la propiedad:");
+        diaFin = scan.nextInt();
+        mesFin = scan.nextInt();
+        añoFin = scan.nextBoolean();
+        Cliente nuevoCliente = new Cliente(nombre,apellido,email,medioDePago,cantDePersonas);
+        nuevoCliente.asignarFecha(diaInicio,diaFin,mesInicio,mesFin,añoInicio,añoFin);
+        return nuevoCliente;
     }
 }
