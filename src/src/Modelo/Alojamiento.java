8fb6a1b7c2f84e9ab45d6efdb3aa250df42881de
Matias Antonelli <matiasantonelli6874@gmail.com>
2024-06-03 18:46:11 -0300

    cambios

--- src/src/Modelo/Alojamiento.java
+++ src/src/Modelo/Alojamiento.java
@@ -189,9 +189,13 @@ public abstract class Alojamiento implements Serializable  {
         valoracion= (valoracion + nuevaValoracion) / cantReservas;
     }
 
+
+
+
+
     public void agregarComentario(String comentario) {
         if (!this.comentarios.isEmpty()) {
-            this.comentarios += "\n"; //deja un espacio para que se separen los comentarios
+            this.comentarios += "\n";
         }
         this.comentarios += comentario;
     }
